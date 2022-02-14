f209520a0401fd85a624342ec0c771f62837f3e6
jiaQiu9 <351470636@qq.com>
2022-02-14 16:16:15 -0500

    update: file addition from the remote upsteam

--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -5,12 +5,17 @@ def euclidean_dist(x, y):
     return res**(1/2)
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    res =0
+    for i in range(len(x)):
+        res+= (x[i] - y[i])
+    return res
 
 def jaccard_dist(x, y):
+    #return 0
     raise NotImplementedError()
 
 def cosine_sim(x, y):
+    #return 0
     raise NotImplementedError()
 
 # Feel free to add more
